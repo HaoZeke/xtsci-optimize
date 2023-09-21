f6892dee70af6c68dcd9652de4bee45a51e77fdf
Rohit Goswami <rog32@hi.is>
2023-09-21 23:24:55 +0000

    MAINT: Initial commit from xtsci-linalg

---
+++ CppCore/tiny_cli.cpp
@@ -0,0 +1,20 @@
+// MIT License
+// Copyright 2023--present Rohit Goswami <HaoZeke>
+#include <fmt/format.h>
+#include <fmt/ostream.h>
+
+#include <cmath>
+#include <cstdlib>
+
+#include <iostream>
+
+#include "include/xtensor_fmt.hpp"
+#include "xtensor-io/xnpz.hpp"
+#include "xtensor/xarray.hpp"
+
+int main(int argc, char *argv[]) {
+  // Eat warnings, also safer
+  static_cast<void>(argc);
+  static_cast<void>(argv);
+  return EXIT_SUCCESS;
+}
