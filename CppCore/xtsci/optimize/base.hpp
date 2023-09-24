a7167ed512b85ee231ea5192162e12ddbae684b0
Rohit Goswami <rog32@hi.is>
2023-09-24 01:27:26 +0000

    ENH: Add basic structures for optimize

---
+++ CppCore/xtsci/optimize/base.hpp
@@ -0,0 +1,68 @@
+#pragma once
+// MIT License
+// Copyright 2023--present Rohit Goswami <HaoZeke>
+#include <algorithm>
+#include <functional>
+#include <limits>
+#include <optional>
+#include <string>
+#include <vector>
+
+#include "xtensor/xarray.hpp"
+
+namespace xts {
+namespace optimize {
+
+template <typename ScalarType = double> struct OptimizeResult {
+  xt::xarray<ScalarType> x; // solution tensor
+  bool success;             // whether or not the optimizer exited successfully
+  int status;               // termination status of the optimizer
+  std::string message;      // description of the termination
+  ScalarType fun;           // value of objective function at the solution
+  xt::xarray<ScalarType> jac;      // value of the Jacobian at the solution
+  xt::xarray<ScalarType> hess;     // value of the Hessian at the solution
+  xt::xarray<ScalarType> hess_inv; // inverse of the Hessian at the solution
+  size_t nfev;      // number of evaluations of the objective functions
+  size_t njev;      // number of evaluations of the Jacobian
+  size_t nhev;      // number of evaluations of the Hessian
+  size_t nit;       // number of iterations performed by the optimizer
+  ScalarType maxcv; // the maximum constraint violation
+};
+
+template <typename ScalarType = double> class ObjectiveFunction {
+public:
+  virtual ~ObjectiveFunction() = default;
+
+  virtual ScalarType operator()(const xt::xarray<ScalarType> &x) const = 0;
+
+  virtual std::optional<xt::xarray<ScalarType>>
+  gradient(const xt::xarray<ScalarType> &x) const {
+    return std::nullopt;
+  }
+
+  virtual std::optional<xt::xarray<ScalarType>>
+  hessian(const xt::xarray<ScalarType> &x) const {
+    return std::nullopt;
+  }
+};
+
+template <typename ScalarType = double> struct OptimizeControl {
+  const size_t max_iterations; // Maximum number of iterations
+  const ScalarType tol;        // Tolerance for termination
+  const bool verbose;          // Whether or not to print progress
+};
+
+template <typename ScalarType = double> class AbstractOptimizer {
+public:
+  // Virtual destructor
+  virtual ~AbstractOptimizer() = default;
+
+  // Pure virtual function for optimization
+  virtual OptimizeResult<ScalarType>
+  optimize(const ObjectiveFunction<ScalarType> &func,
+           const xt::xexpression<ScalarType> &initial_guess,
+           const OptimizeControl<ScalarType> &control) const = 0;
+};
+
+} // namespace optimize
+} // namespace xts
---
+++ CppCore/xtsci/optimize/linesearch/base.hpp
@@ -0,0 +1,48 @@
+#pragma once
+// MIT License
+// Copyright 2023--present Rohit Goswami <HaoZeke>
+#include <algorithm>
+#include <functional>
+#include <limits>
+#include <string>
+#include <vector>
+
+#include "xtsci/optimize/base.hpp"
+
+namespace xts {
+namespace optimize {
+namespace linesearch {
+
+template <typename ScalarType> class LineSearchCondition {
+public:
+  virtual bool operator()(ScalarType alpha,
+                          const ObjectiveFunction<ScalarType> &func,
+                          const xt::xarray<ScalarType> &x,
+                          const xt::xarray<ScalarType> &direction) const = 0;
+};
+
+template <typename ScalarType> class LineSearchStrategy {
+public:
+  virtual ScalarType
+  search(const ObjectiveFunction<ScalarType> &func,
+         const xt::xarray<ScalarType> &x,
+         const xt::xarray<ScalarType> &direction,
+         const LineSearchCondition<ScalarType> &condition) const = 0;
+};
+
+template <typename ScalarType>
+class LineSearchOptimizer : public AbstractOptimizer<ScalarType> {
+  virtual OptimizeResult<ScalarType>
+  optimize(const ObjectiveFunction<ScalarType> &func,
+           const xt::xexpression<ScalarType> &initial_guess,
+           const OptimizeControl<ScalarType> &control) const = 0;
+protected:
+  const LineSearchCondition<ScalarType>
+      m_ls_cond; // Condition for step size acceptance
+  const LineSearchStrategy<ScalarType>
+      m_ls_strat; // Strategy for finding optimal step size
+};
+
+} // namespace linesearch
+} // namespace optimize
+} // namespace xts
