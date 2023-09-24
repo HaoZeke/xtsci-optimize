0fdd33f0b4ad46440a8e62efb13c9dd2b63e43b9
Rohit Goswami <rog32@hi.is>
2023-09-24 22:57:24 +0000

    ENH: Add a basic SR1

--- CppCore/tiny_cli.cpp
+++ CppCore/tiny_cli.cpp
@@ -31,6 +31,7 @@
 #include "xtsci/optimize/minimize/bfgs.hpp"
 #include "xtsci/optimize/minimize/cg.hpp"
 #include "xtsci/optimize/minimize/lbfgs.hpp"
+#include "xtsci/optimize/minimize/sr1.hpp"
 
 #include "xtsci/optimize/trial_functions/quadratic.hpp"
 #include "xtsci/optimize/trial_functions/rosenbrock.hpp"
@@ -95,12 +96,13 @@ int main(int argc, char *argv[]) {
   xts::optimize::minimize::BFGSOptimizer<double> bfgsopt(backtracking);
   xts::optimize::minimize::LBFGSOptimizer<double> lbfgsopt(backtracking, 10);
   xts::optimize::minimize::ADAMOptimizer<double> adaopt(backtracking);
+  xts::optimize::minimize::SR1Optimizer<double> sr1opt(zoom);
 
   xt::xarray<double> initial_guess = {-1.3, 1.8};
   xt::xarray<double> direction = {0.0, 0.0};
   xts::optimize::SearchState<double> cstate = {initial_guess, direction};
   xts::optimize::OptimizeResult<double> result =
-      adaopt.optimize(rosen, cstate, control);
+      sr1opt.optimize(rosen, cstate, control);
 
   std::cout << "Optimized x: " << result.x << "\n";
   std::cout << "Function value: " << result.fun << "\n";
---
+++ CppCore/xtsci/optimize/minimize/sr1.hpp
@@ -0,0 +1,102 @@
+#pragma once
+// MIT License
+// Copyright 2023--present Rohit Goswami <HaoZeke>
+#include <fmt/ostream.h>
+
+#include "xtensor/xnoalias.hpp"
+
+#include "xtsci/optimize/linesearch/base.hpp"
+
+#include "xtensor-blas/xlinalg.hpp"
+
+namespace xts {
+namespace optimize {
+namespace minimize {
+
+template <typename ScalarType>
+class SR1Optimizer : public linesearch::LineSearchOptimizer<ScalarType> {
+public:
+  explicit SR1Optimizer(linesearch::LineSearchStrategy<ScalarType> &strategy)
+      : linesearch::LineSearchOptimizer<ScalarType>(strategy) {}
+
+  OptimizeResult<ScalarType>
+  optimize(const ObjectiveFunction<ScalarType> &func,
+           const SearchState<ScalarType> &initial_guess,
+           const OptimizeControl<ScalarType> &control) const override {
+    auto [x, dir_] = initial_guess;
+
+    auto grad_opt = func.gradient(x);
+    if (!grad_opt) {
+      throw std::runtime_error("Gradient required for SR1 method.");
+    }
+
+    auto gradient = *grad_opt;
+    xt::xarray<ScalarType> B = xt::eye({x.size(), x.size()}, 0);
+
+    if (control.verbose) {
+      fmt::print("Initial x: {}\n", x);
+      fmt::print("Initial gradient: {}\n", gradient);
+    }
+
+    OptimizeResult<ScalarType> result;
+
+    xt::xarray<ScalarType> term({x.size(), x.size()});
+
+    for (result.nit = 0; result.nit < control.max_iterations; ++result.nit) {
+      if (control.verbose) {
+        fmt::print("Iteration: {}\n", result.nit);
+      }
+
+      auto direction = -xt::linalg::dot(
+          xt::linalg::inv(B),
+          gradient); // Note: You may consider improving the efficiency of this
+                     // by not computing the inverse directly.
+      ScalarType alpha = this->m_ls_strat.search(func, {x, direction});
+      if (control.verbose) {
+        fmt::print("Alpha: {}\n", alpha);
+      }
+
+      auto s = alpha * direction;
+      xt::noalias(x) += s;
+
+      if (control.verbose) {
+        fmt::print("x: {}\n", x);
+      }
+
+      auto prev_gradient = gradient;
+      auto new_grad_opt = func.gradient(x);
+      if (!new_grad_opt) {
+        throw std::runtime_error("Gradient required for SR1 method.");
+      }
+
+      gradient = *new_grad_opt;
+      auto y = gradient - prev_gradient;
+
+      // TODO(rgoswami): Maybe LDLT or something later?
+      auto B_s = xt::linalg::dot(B, s);
+      auto delta = y - B_s;
+      xt::noalias(term) =
+          xt::linalg::outer(delta, delta) / xt::linalg::dot(delta, s)();
+
+      B += term;
+
+      if (control.verbose) {
+        fmt::print("New gradient: {}\n", gradient);
+      }
+
+      if (xt::amax(xt::abs(gradient))() < control.tol) {
+        break;
+      }
+    }
+
+    result.x = x;
+    result.fun = func(x);
+    result.jac = gradient;
+
+    return result;
+  }
+};
+
+} // namespace minimize
+} // namespace optimize
+} // namespace xts
