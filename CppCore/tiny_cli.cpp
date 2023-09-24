7189558c5b4f9ef43e92731366cfa585645f3a38
Rohit Goswami <rog32@hi.is>
2023-09-24 03:41:04 +0000

    ENH: Demonstrate an example of rosenbrock and cg

--- CppCore/tiny_cli.cpp
+++ CppCore/tiny_cli.cpp
@@ -9,36 +9,64 @@
 #include <iostream>
 
 #include "include/xtensor_fmt.hpp"
-#include "include/xtensor_trial_funcs.hpp"
 #include "xtensor-io/xnpz.hpp"
 #include "xtensor/xarray.hpp"
 
-using namespace xts::funcs::trial;
+#include "xtsci/optimize/base.hpp"
+#include "xtsci/optimize/linesearch/conditions/armijo.hpp"
+#include "xtsci/optimize/linesearch/search_strategy/backtracking.hpp"
+#include "xtsci/optimize/linesearch/search_strategy/bisection.hpp"
+#include "xtsci/optimize/minimize/cg.hpp"
+#include "xtsci/optimize/trial_functions/quadratic.hpp"
+#include "xtsci/optimize/trial_functions/rosenbrock.hpp"
+
 int main(int argc, char *argv[]) {
   // Eat warnings, also safer
   static_cast<void>(argc);
   static_cast<void>(argv);
-  // Define the grid
-  size_t n_points = 100; // Number of points along each axis
-  double x_min = -2.0, x_max = 2.0;
-  double y_min = -2.0, y_max = 2.0;
+  // // Define the grid
+  // size_t n_points = 100; // Number of points along each axis
+  // double x_min = -2.0, x_max = 2.0;
+  // double y_min = -2.0, y_max = 2.0;
 
-    // Generate grid
-    xt::xtensor<double, 1> x = xt::linspace<double>(-2.0, 2.0, 100);
-    xt::xtensor<double, 1> y = xt::linspace<double>(-2.0, 2.0, 100);
+  // // Generate grid
+  // xt::xtensor<double, 1> x = xt::linspace<double>(-2.0, 2.0, 100);
+  // xt::xtensor<double, 1> y = xt::linspace<double>(-2.0, 2.0, 100);
 
-    xt::xtensor<double, 2> X, Y;
-    std::tie(X, Y) = xt::meshgrid(x, y);
+  // xt::xtensor<double, 2> X, Y;
+  // std::tie(X, Y) = xt::meshgrid(x, y);
 
-    // Evaluate Rosenbrock function on the grid
-    xt::xtensor<double, 2> Z = rosenbrock<double>(X, Y);
+  // // Evaluate Rosenbrock function on the grid
+  // xt::xtensor<double, 2> Z = rosenbrock<double>(X, Y);
 
-    // Save to disk
-    // Write data to NPZ
-    xt::dump_npz("rosenbrock.npz", "X", X);
-    xt::dump_npz("rosenbrock.npz", "Y", Y, true); // The 'true' means append to existing file
-    xt::dump_npz("rosenbrock.npz", "Z", Z, true);
+  // // Save to disk
+  // // Write data to NPZ
+  // xt::dump_npz("rosenbrock.npz", "X", X);
+  // xt::dump_npz("rosenbrock.npz", "Y", Y,
+  //              true); // The 'true' means append to existing file
+  // xt::dump_npz("rosenbrock.npz", "Z", Z, true);
 
-    std::cout << "Data written to rosenbrock.npz" << std::endl;
+  // std::cout << "Data written to rosenbrock.npz" << std::endl;
+
+  // Use a minimizer
+  xts::optimize::trial_functions::Rosenbrock<double> rosen;
+  xts::optimize::trial_functions::QuadraticFunction<double> quadratic;
+  xts::optimize::OptimizeControl<double> control;
+  xts::optimize::linesearch::conditions::ArmijoCondition<double> armijo;
+  xts::optimize::linesearch::search_strategy::BisectionSearch<double> bisection;
+  xts::optimize::linesearch::search_strategy::BacktrackingSearch<double>
+      backtracking;
+  xts::optimize::minimize::ConjugateGradientOptimizer<double> optimizer(
+      armijo, backtracking);
+
+  xt::xarray<double> initial_guess = {3.0, 4.0};
+  xt::xarray<double> direction = {-1.0, -1.0};
+  xts::optimize::SearchState<double> cstate = {initial_guess, direction};
+  xts::optimize::OptimizeResult<double> result =
+      optimizer.optimize(rosen, cstate, control);
+
+  std::cout << "Optimized x: " << result.x << "\n";
+  std::cout << "Function value: " << result.fun << "\n";
+  std::cout << "Number of iterations: " << result.nit << "\n";
   return EXIT_SUCCESS;
 }
