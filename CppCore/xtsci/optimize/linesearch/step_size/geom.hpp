6bfa63f6eb19c472283c930b042f9968d5f0630f
Rohit Goswami <rog32@hi.is>
2023-09-24 20:52:51 +0000

    ENH: Add a basic geometric reduction step

---
+++ CppCore/xtsci/optimize/linesearch/step_size/geom.hpp
@@ -0,0 +1,34 @@
+#pragma once
+// MIT License
+// Copyright 2023--present Rohit Goswami <HaoZeke>
+#include <algorithm>
+#include <functional>
+#include <limits>
+#include <string>
+#include <vector>
+
+#include "xtsci/optimize/base.hpp"
+#include "xtsci/optimize/linesearch/base.hpp"
+
+namespace xts {
+namespace optimize {
+namespace linesearch {
+namespace step_size {
+template <typename ScalarType>
+class GeometricReductionStepSize : public StepSizeStrategy<ScalarType> {
+private:
+  ScalarType beta;
+
+public:
+  explicit GeometricReductionStepSize(ScalarType b = 0.5) : beta(b) {}
+
+  ScalarType nextStep(ScalarType, ScalarType alpha_hi,
+                      const ObjectiveFunction<ScalarType> &,
+                      const SearchState<ScalarType> &) const override {
+    return alpha_hi * beta;
+  }
+};
+} // namespace step_size
+} // namespace linesearch
+} // namespace optimize
+} // namespace xts
