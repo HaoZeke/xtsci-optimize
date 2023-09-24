e5756928cbb69e11d0ca54568805319d18689fa5
Rohit Goswami <rog32@hi.is>
2023-09-24 20:02:03 +0000

    MAINT: Expand the size of the grid

--- scripts/plot_cpp_cg.py
+++ scripts/plot_cpp_cg.py
@@ -19,7 +19,7 @@ num_evaluations_func = num_evaluations_grad  # Use gradient evaluations as a pro
 
 # Create meshgrid
 x = np.linspace(-2, 4, 400)
-y = np.linspace(-2, 6, 400)
+y = np.linspace(-2, 8, 400)
 X, Y = np.meshgrid(x, y)
 Z = (1.0 - X) ** 2 + 100.0 * (Y - X**2) ** 2
 
